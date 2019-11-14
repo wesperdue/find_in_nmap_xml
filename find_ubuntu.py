06a52f946431c41fdf66beb9ecc4339c981f9572
Wesley Perdue <wes@wesleyt.net>
2019-11-13 18:26:12 -0800

    check for bad filenames

--- find_ubuntu.py
+++ find_ubuntu.py
@@ -3,6 +3,7 @@
 if __name__ == '__main__':  # pragma: no cover
   import sys
   import xml.etree.ElementTree as ET
+  import os.path
 
   if len(sys.argv)==1:
     print('Usage:\n',sys.argv[0],' <input_file>')
@@ -10,15 +11,17 @@ if __name__ == '__main__':  # pragma: no cover
 
   for x in range(1,len(sys.argv)):
     infile_name = sys.argv[x]
-    # print(x,sys.argv[x])
-    tree=ET.parse(infile_name)
-    root=tree.getroot()
-      
-    for host in root:
-      for address in host.findall('address'):
-        ipaddr=str(address.get('addr'))
-        for service in host.findall("ports/port/service"):
-          ver=str(service.get('extrainfo'))
-          if(ver.find("Ubuntu")!=-1):
-            # print(f"{ipaddr}:  Ubuntu")
-            print(ipaddr)
+    if os.path.isfile(infile_name):
+      tree=ET.parse(infile_name)
+      root=tree.getroot()
+        
+      for host in root:
+        for address in host.findall('address'):
+          ipaddr=str(address.get('addr'))
+          for service in host.findall("ports/port/service"):
+            ver=str(service.get('extrainfo'))
+            if(ver.find("Ubuntu")!=-1):
+              # print(f"{ipaddr}:  Ubuntu")
+              print(ipaddr)
+    else:
+      print("Error: File "+infile_name+" not found")
