470b0c5e0bf7c1b0ff1ddd424478276abf84526d
Wesley Perdue <wes@wesleyt.net>
2019-11-13 18:06:05 -0800

    switched field checked for Ubuntu

--- find_ubuntu.py
+++ find_ubuntu.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':  # pragma: no cover
       for address in host.findall('address'):
         ipaddr=str(address.get('addr'))
         for service in host.findall("ports/port/service"):
-          ver=str(service.get('version'))
+          ver=str(service.get('extrainfo'))
           if(ver.find("Ubuntu")!=-1):
             # print(f"{ipaddr}:  Ubuntu")
             print(ipaddr)
