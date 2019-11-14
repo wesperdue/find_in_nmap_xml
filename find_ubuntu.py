39fa608e86170b69f7738f8a5058af1a2e511671
Wesley Perdue <wes@wesleyt.net>
2019-11-13 17:48:15 -0800

    process multiple files on the command line

--- find_ubuntu.py
+++ find_ubuntu.py
@@ -7,28 +7,18 @@ if __name__ == '__main__':  # pragma: no cover
   if len(sys.argv)==1:
     print('Usage:\n',sys.argv[0],' <input_file>')
     exit(0)
-  
-  infile_name = sys.argv[1]
-  tree=ET.parse(infile_name)
-  root=tree.getroot()
 
-# for child in root:
-#   print(child.tag, child.attrib)
-
-# for address in root.findall("./host/address"):
-#   print(address.get('addr'))
-#   
-# for service in root.findall("./host/ports/port/service"):
-#   print(service.get('version'))
-#   
-# for host in root:
-#   for service in host.findall("ports/port/service"):
-#     print(service.get('version'))
+  for x in range(1,len(sys.argv)):
+    infile_name = sys.argv[x]
+    # print(x,sys.argv[x])
+    tree=ET.parse(infile_name)
+    root=tree.getroot()
       
-  for host in root:
-    for address in host.findall('address'):
-      ipaddr=str(address.get('addr'))
-      for service in host.findall("ports/port/service"):
-        ver=str(service.get('version'))
-        if(ver.find("Ubuntu")!=-1):
-          print(f"{ipaddr}:  Ubuntu")
+    for host in root:
+      for address in host.findall('address'):
+        ipaddr=str(address.get('addr'))
+        for service in host.findall("ports/port/service"):
+          ver=str(service.get('version'))
+          if(ver.find("Ubuntu")!=-1):
+            # print(f"{ipaddr}:  Ubuntu")
+            print(ipaddr)
